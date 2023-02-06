d56608dbc11634b86bd38cfd2a7c571d48e26337
wangmingming <1960588251@qq.com>
2023-02-06 16:45:44 +0800

    update

--- main.go
+++ main.go
@@ -88,36 +88,36 @@ type Questionnaire struct {
 	Name       string    `gorm:"column:name;not null;size:256" db:"name" json:"name" form:"name" binding:"required"`
 	Result     string    `gorm:"column:result;not null;size:256" db:"result" json:"result" form:"result"`
 	CreatedAt  time.Time `gorm:"column:created_at" db:"created_at" json:"created_at" form:"created_at"`
-	Question1  uint8     `gorm:"column:question_1" db:"question_1" json:"question_1" form:"question_1"`
-	Question2  uint8     `gorm:"column:question_2" db:"question_2" json:"question_2" form:"question_2"`
-	Question3  uint8     `gorm:"column:question_3" db:"question_3" json:"question_3" form:"question_3"`
-	Question4  uint8     `gorm:"column:question_4" db:"question_4" json:"question_4" form:"question_4"`
-	Question5  uint8     `gorm:"column:question_5" db:"question_5" json:"question_5" form:"question_5"`
-	Question6  uint8     `gorm:"column:question_6" db:"question_6" json:"question_6" form:"question_6"`
-	Question7  uint8     `gorm:"column:question_7" db:"question_7" json:"question_7" form:"question_7"`
-	Question8  uint8     `gorm:"column:question_8" db:"question_8" json:"question_8" form:"question_8"`
-	Question9  uint8     `gorm:"column:question_9" db:"question_9" json:"question_9" form:"question_9"`
-	Question10 uint8     `gorm:"column:question_10" db:"question_10" json:"question_10" form:"question_10"`
-	Question11 uint8     `gorm:"column:question_11" db:"question_11" json:"question_11" form:"question_11"`
-	Question12 uint8     `gorm:"column:question_12" db:"question_12" json:"question_12" form:"question_12"`
-	Question13 uint8     `gorm:"column:question_13" db:"question_13" json:"question_13" form:"question_13"`
-	Question14 uint8     `gorm:"column:question_14" db:"question_14" json:"question_14" form:"question_14"`
-	Question15 uint8     `gorm:"column:question_15" db:"question_15" json:"question_15" form:"question_15"`
-	Question16 uint8     `gorm:"column:question_16" db:"question_16" json:"question_16" form:"question_16"`
-	Question17 uint8     `gorm:"column:question_17" db:"question_17" json:"question_17" form:"question_17"`
-	Question18 uint8     `gorm:"column:question_18" db:"question_18" json:"question_18" form:"question_18"`
-	Question19 uint8     `gorm:"column:question_19" db:"question_19" json:"question_19" form:"question_19"`
-	Question20 uint8     `gorm:"column:question_20" db:"question_20" json:"question_20" form:"question_20"`
-	Question21 uint8     `gorm:"column:question_21" db:"question_21" json:"question_21" form:"question_21"`
-	Question22 uint8     `gorm:"column:question_22" db:"question_22" json:"question_22" form:"question_22"`
-	Question23 uint8     `gorm:"column:question_23" db:"question_23" json:"question_23" form:"question_23"`
-	Question24 uint8     `gorm:"column:question_24" db:"question_24" json:"question_24" form:"question_24"`
-	Question25 uint8     `gorm:"column:question_25" db:"question_25" json:"question_25" form:"question_25"`
-	Question26 uint8     `gorm:"column:question_26" db:"question_26" json:"question_26" form:"question_26"`
-	Question27 uint8     `gorm:"column:question_27" db:"question_27" json:"question_27" form:"question_27"`
-	Question28 uint8     `gorm:"column:question_28" db:"question_28" json:"question_28" form:"question_28"`
-	Question29 uint8     `gorm:"column:question_29" db:"question_29" json:"question_29" form:"question_29"`
-	Question30 uint8     `gorm:"column:question_30" db:"question_30" json:"question_30" form:"question_30"`
+	Question1  uint8     `gorm:"column:question_1" db:"question_1" json:"question_1,string" form:"question_1"`
+	Question2  uint8     `gorm:"column:question_2" db:"question_2" json:"question_2,string" form:"question_2"`
+	Question3  uint8     `gorm:"column:question_3" db:"question_3" json:"question_3,string" form:"question_3"`
+	Question4  uint8     `gorm:"column:question_4" db:"question_4" json:"question_4,string" form:"question_4"`
+	Question5  uint8     `gorm:"column:question_5" db:"question_5" json:"question_5,string" form:"question_5"`
+	Question6  uint8     `gorm:"column:question_6" db:"question_6" json:"question_6,string" form:"question_6"`
+	Question7  uint8     `gorm:"column:question_7" db:"question_7" json:"question_7,string" form:"question_7"`
+	Question8  uint8     `gorm:"column:question_8" db:"question_8" json:"question_8,string" form:"question_8"`
+	Question9  uint8     `gorm:"column:question_9" db:"question_9" json:"question_9,string" form:"question_9"`
+	Question10 uint8     `gorm:"column:question_10" db:"question_10" json:"question_10,string" form:"question_10"`
+	Question11 uint8     `gorm:"column:question_11" db:"question_11" json:"question_11,string" form:"question_11"`
+	Question12 uint8     `gorm:"column:question_12" db:"question_12" json:"question_12,string" form:"question_12"`
+	Question13 uint8     `gorm:"column:question_13" db:"question_13" json:"question_13,string" form:"question_13"`
+	Question14 uint8     `gorm:"column:question_14" db:"question_14" json:"question_14,string" form:"question_14"`
+	Question15 uint8     `gorm:"column:question_15" db:"question_15" json:"question_15,string" form:"question_15"`
+	Question16 uint8     `gorm:"column:question_16" db:"question_16" json:"question_16,string" form:"question_16"`
+	Question17 uint8     `gorm:"column:question_17" db:"question_17" json:"question_17,string" form:"question_17"`
+	Question18 uint8     `gorm:"column:question_18" db:"question_18" json:"question_18,string" form:"question_18"`
+	Question19 uint8     `gorm:"column:question_19" db:"question_19" json:"question_19,string" form:"question_19"`
+	Question20 uint8     `gorm:"column:question_20" db:"question_20" json:"question_20,string" form:"question_20"`
+	Question21 uint8     `gorm:"column:question_21" db:"question_21" json:"question_21,string" form:"question_21"`
+	Question22 uint8     `gorm:"column:question_22" db:"question_22" json:"question_22,string" form:"question_22"`
+	Question23 uint8     `gorm:"column:question_23" db:"question_23" json:"question_23,string" form:"question_23"`
+	Question24 uint8     `gorm:"column:question_24" db:"question_24" json:"question_24,string" form:"question_24"`
+	Question25 uint8     `gorm:"column:question_25" db:"question_25" json:"question_25,string" form:"question_25"`
+	Question26 uint8     `gorm:"column:question_26" db:"question_26" json:"question_26,string" form:"question_26"`
+	Question27 uint8     `gorm:"column:question_27" db:"question_27" json:"question_27,string" form:"question_27"`
+	Question28 uint8     `gorm:"column:question_28" db:"question_28" json:"question_28,string" form:"question_28"`
+	Question29 uint8     `gorm:"column:question_29" db:"question_29" json:"question_29,string" form:"question_29"`
+	Question30 uint8     `gorm:"column:question_30" db:"question_30" json:"question_30,string" form:"question_30"`
 }
 
 // 把第5、10、14、18、24、30题的分加起来就是你的“老虎”分数
